8fe909ca83586b9f6138bc05beb04502197d33f0
jsamuelsen <jsamuelsen11@gmail.com>
2026-03-08 00:41:26 -0500

    chore(release): v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
 
   defaultConfig {
     applicationId = "com.jsamuelsen11.daykeeper.app"
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.0.1"
   }
 
   buildFeatures { compose = true }
